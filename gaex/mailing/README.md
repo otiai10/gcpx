4b0c8226bf85e60834f54408d26f0c8a8ad14927
otiai10 <otiai10@gmail.com>
2016-04-02 16:52:03 +0200

    Add mailing example

---
+++ gaex/mailing/README.md
@@ -0,0 +1,4 @@
+```
+% cd $GOPATH/src/github.com/otiai10/gcpx/gaex/gaex
+% appcfg.py -E GAEX_MAIL_SENDER:{YOUR_EMAIL} -A {YOUR_APP_NAME} -V v1 update ./
+```
---
+++ gaex/mailing/main.go
@@ -0,0 +1,56 @@
+package oppai
+
+import (
+	"net/http"
+	"os"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/mail"
+
+	"github.com/otiai10/marmoset"
+)
+
+func init() {
+
+	marmoset.LoadViews("./views")
+
+	router := marmoset.NewRouter()
+
+	router.GET("/", func(w http.ResponseWriter, r *http.Request) {
+		marmoset.Render(w).HTML("index", nil)
+	})
+
+	router.POST("/", func(w http.ResponseWriter, r *http.Request) {
+
+		mailto := r.FormValue("mailto")
+		if mailto == "" {
+			marmoset.Render(w).HTML("index", map[string]interface{}{
+				"error": "Address Undefined",
+			})
+			return
+		}
+
+		ctx := appengine.NewContext(r)
+		msg := &mail.Message{
+			Sender:  os.Getenv("GAEX_MAIL_SENDER"),
+			To:      []string{mailto},
+			Subject: "GAE/Go Mailing Example",
+			Body:    "This is plain text body",
+			HTMLBody: `<h1>This is HTML body</h1><p>
+			<a href='https://github.com/otiai10/gcpx/gaex/mailing'>source code</a>`,
+		}
+
+		if err := mail.Send(ctx, msg); err != nil {
+			marmoset.Render(w).HTML("index", map[string]interface{}{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		marmoset.Render(w).HTML("index", map[string]interface{}{
+			"success": mailto,
+		})
+	})
+
+	http.Handle("/", router)
+}
